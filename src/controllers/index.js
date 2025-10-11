655ae31ef17bc0ff2b50f5afc427da13373d6825
Bolo Michelin <72155+bolom@users.noreply.github.com>
2025-10-11 10:32:28 -0400

    Add AppKit-compatible chain-connect controller for wallet integration

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,6 +1,8 @@
 
 import { Application } from "@hotwired/stimulus"
 import WalletController from "./wallet_controller.js"
+import ChainConnectedController from "./chain_connected_controller.js"
 
 window.Stimulus = Application.start()
 Stimulus.register("wallet", WalletController)
+Stimulus.register("chain-connect", ChainConnectedController)
